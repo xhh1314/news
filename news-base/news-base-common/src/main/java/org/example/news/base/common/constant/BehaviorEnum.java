a6423b9193162fd11c8525c8e0319a1719ca7d36
雕兄 <903440799@qq.com>
2018-02-02 10:42:43 +0800

    做一个常量

---
+++ news-base/news-base-common/src/main/java/org/example/news/base/common/constant/BehaviorEnum.java
@@ -0,0 +1,18 @@
+package org.example.news.base.common.constant;
+
+/**
+ * 用户行为常量
+ * 0 点赞
+ * 1 喜欢
+ * 2 不感兴趣
+ * 3 看过
+ *
+ * @author lh
+ * @date 2018/2/2
+ * @since
+ */
+public enum BehaviorEnum {
+
+   likes,interest,uninterest,seen
+
+}
